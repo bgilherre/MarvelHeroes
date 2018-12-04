6fc2d288b11f562ff20202a2e93beb2705b4cd1e
Borja Gil Herrero <borjagilh@gmail.com>
2018-12-04 10:57:04 +0100

    Codable incluido

---
+++ Heroes Marvel/Modelo/CharactersMarvel.swift
@@ -0,0 +1,108 @@
+//
+//  CharactersMarvel.swift
+//  Heroes Marvel
+//
+//  Created by Borja Gil Herrero on 03/12/2018.
+//  Copyright © 2018 Alsis GHE. All rights reserved.
+//
+
+import Foundation
+
+class RespuestaAPI: NSObject, Codable{
+    var code: Int = 0
+    var status: String = ""
+    var copyright: String = ""
+    var attributionText: String = ""
+    var attributionHTML: String = ""
+    var etag: String = ""
+    var data: DataAPI = DataAPI()
+}
+class DataAPI: NSObject, Codable{
+    var offset: Int = 0
+    var limit: Int = 0
+    var total: Int = 0
+    var count: Int = 0
+    var results: [CharacterAPI]	 = [CharacterAPI]()
+}
+
+class CharacterAPI: NSObject, Codable {
+    enum CodingKeys: String, CodingKey {
+        //case id
+        case name
+        case descriptionString = "description"
+        case modified
+        //case thumbnail
+        case resourceURI
+        //case comics        
+    }
+    var id: Int32 = 0
+    var name: String = ""
+    var descriptionString: String = ""
+    var modified: String = ""
+    var thumbnail: ImagenCharacterAPI = ImagenCharacterAPI()
+    var resourceURI: String = ""
+    var comics: ComicsAPI = ComicsAPI()
+    var series: SeriesAPI = SeriesAPI()
+    var stories: StoriesAPI = StoriesAPI()
+    var events: EventsAPI = EventsAPI()
+    var urls: [UrlAPI] = [UrlAPI]()
+}
+
+class ImagenCharacterAPI:NSObject, Codable{
+    enum CodingKeys: String, CodingKey {
+        case path
+        case extensionString = "extension"
+    }
+    var path: String = ""
+    var extensionString: String = ""
+}
+
+class ComicsAPI: NSObject, Codable{
+    var available: Int = 0
+    var collectionURI: String = ""
+    var items: [ComicAPI] = [ComicAPI]()
+    var returned : Int = 0
+}
+class SeriesAPI: NSObject, Codable{
+    var available: Int = 0
+    var collectionURI: String = ""
+    var items: [SerieAPI] = [SerieAPI]()
+    var returned: Int = 0
+}
+class StoriesAPI: NSObject, Codable{
+    var available: Int = 0
+    var collectionURI: String = ""
+    var items: [StorieAPI] = [StorieAPI]()
+    var returned: Int = 0
+}
+class EventsAPI: NSObject, Codable{
+    var available: Int = 0
+    var collectionURI: String = ""
+    var items: [EventAPI] = [EventAPI]()
+    var returned: Int = 0
+}
+
+class ComicAPI: NSObject, Codable{
+    var resourceURI: String = ""
+    var name: String = ""
+}
+
+class SerieAPI: NSObject, Codable{
+    var resourceURI: String = ""
+    var name: String = ""
+}
+
+class StorieAPI: NSObject, Codable{
+    var resourceURI: String = ""
+    var name: String = ""
+    var type: String = ""
+}
+class EventAPI: NSObject, Codable{
+    var resourceURI: String = ""
+    var name: String = ""
+}
+class UrlAPI: NSObject, Codable{
+    var type: String = ""
+    var url: String = ""
+}
+
--- Heroes Marvel/Red/MarvelRed.swift
+++ Heroes Marvel/Red/MarvelRed.swift
@@ -48,20 +48,37 @@ class MarvelRed: NSObject{
                 let result = NSString(data: data!, encoding: String.Encoding.ascii.rawValue)!
                 let cadData:String = (result as String)
 
-                let dataA : Data = cadData.data(using: String.Encoding.utf8)!
+                //let dataA : Data = cadData.data(using: String.Encoding.utf8)!
                 //Transformamos la informacion a un diccionario
 
-                let dataB : NSDictionary = try JSONSerialization.jsonObject(with: dataA, options:JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
-                //Recorremos la informacion que nos devuelve la API de Marvel para llegar a la información
-                let code: [Int64] = dataB.objects(forKeys: ["code"], notFoundMarker: self) as! [Int64]
-                if code[0] == 200{
-                    let datos:[NSDictionary]  = dataB.objects(forKeys: ["data"], notFoundMarker: self) as! [NSDictionary]
-            
-                    let resultados: [NSArray] = datos.first!.objects(forKeys: ["results"], notFoundMarker: self) as! [NSArray]
+                //let dataB : NSDictionary = try JSONSerialization.jsonObject(with: dataA, options:JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+                
+                if let jsonData = cadData.data(using: .utf8)
+                {
+                    let respuestaAPI = try? JSONDecoder().decode(RespuestaAPI.self, from: jsonData)
+                    //Only with name, age, gender properties decoded from json as we have defined CodingKeys enum in Person class.
+                    //while respuestaAPI == nil{
+                   // }
+                    print(respuestaAPI?.data.results[2].name)
+                    print(respuestaAPI?.code)
                     
-                    if resultados.count != 0{
-                        if resultados[0].count != 0{
-                            crearPersonajes(datos: resultados[0])
+
+                    //person.phone and person.country will be empty
+                
+                //Recorremos la informacion que nos devuelve la API de Marvel para llegar a la información
+                ///*
+                //let code: [Int64] = dataB.objects(forKeys: ["code"], notFoundMarker: self) as! [Int64]
+                    if respuestaAPI!.code == 200{
+                //if code[0] == 200{
+                    //let datos:[NSDictionary]  = dataB.objects(forKeys: ["data"], notFoundMarker: self) as! [NSDictionary]
+            
+                    //let resultados: [NSArray] = datos.first!.objects(forKeys: ["results"], notFoundMarker: self) as! [NSArray]
+                        if respuestaAPI!.data.count != 0{
+                    //if resultados.count != 0{
+                            if respuestaAPI!.data.results.count != 0{
+                        //if resultados[0].count != 0{
+                            //crearPersonajes(datos: resultados[0])
+                                crearPersonajes(datos: respuestaAPI!.data.results)
                             retorno =  true
                             print("entro2 :" + String(retorno))
                         }else{
@@ -72,8 +89,12 @@ class MarvelRed: NSObject{
                     }
                 }else{
                     retorno = false
-                    print("Codigo de error: " + String(code[0]))
+                        print("Codigo de error: " + (respuestaAPI?.code.description)!)
                 }
+                }else{
+                    retorno = false
+                }
+ 
             }catch{
                 print(error)
                 semaforo.leave()
@@ -86,17 +107,18 @@ class MarvelRed: NSObject{
 
     }
 
-    class func crearPersonajes(datos: NSArray){
+    class func crearPersonajes(datos: [CharacterAPI]){
 
         for item in datos{
             
-            let nombre: String = (item as AnyObject).value(forKey: "name") as! String
-            let descripcion : String = (item as AnyObject).value(forKey: "description") as! String
-            let thumbnail = (item as AnyObject).value(forKey: "thumbnail")
-            let rutaImagen: String = (thumbnail as AnyObject).value(forKey: "path") as! String
-            let extensionImagen: String = (thumbnail as AnyObject).value(forKey: "extension") as! String
+            let nombre: String = item.name
+            let descripcion :String = item.descriptionString
+            //let thumbnail = (item as AnyObject).value(forKey: "thumbnail")
+            let rutaImagen: String = item.thumbnail.path
+            let extensionImagen: String = item.thumbnail.extensionString
             let rutaCompleta: String = rutaImagen + "." + extensionImagen
-            
+            //let rutaCompleta: String = rutaImagen + "." + "jpg"
+            print(rutaCompleta)
             crearHeroe(nombre: nombre, descripcion: descripcion, imagen: rutaCompleta)
         }
 
